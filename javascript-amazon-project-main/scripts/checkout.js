52dd69741882445ef890cc46c15bcfc73150ef2c
Kishik <kishikkhajuria03@gmail.com>
2026-03-21 16:24:04 +0530

    Using dayjs() external library for dates.

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -6,6 +6,11 @@ import {
 } from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
+
+const today = dayjs();
+const deliveryDate = today.add(7, 'day');
+console.log(deliveryDate.format('dddd, MMMM D'));
+
 let cartSummaryHTML = '';
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
