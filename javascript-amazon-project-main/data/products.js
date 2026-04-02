2b78d2a23414be76000ceb73722b5ef9839c4cc3
Kishik <kishikkhajuria03@gmail.com>
2026-04-02 17:40:52 +0530

    convert products into classes

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from '../scripts/utils/money.js';
+
 export function getProduct(productId){
   let matchingProduct;
     products.forEach((product) => {
@@ -7,6 +9,38 @@ export function getProduct(productId){
     });
     return matchingProduct;
 };
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  keywords;
+  type;
+  sizeChartLink;
+
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords;
+    this.type = productDetails.type;
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${Math.round(this.rating.stars * 10)}.png`;
+  }
+
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+}
+
+
 
 export const products = [
   {
@@ -667,4 +701,7 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+return new Product(productDetails);
+});
+
