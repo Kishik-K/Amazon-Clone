cae6caa34284b22f582da8b79560398f1e9d916d
Kishik <kishikkhajuria03@gmail.com>
2026-04-04 23:33:46 +0530

    create order using the back-end

--- javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -2,14 +2,15 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
-export function renderPaymentSummary(){ 
+export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
 
   cart.forEach((cartItem) => {
     const product = getProduct(cartItem.productId);
-    productPriceCents += product.priceCents * cartItem.quantity
+    productPriceCents += product.priceCents * cartItem.quantity;
 
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
@@ -18,7 +19,7 @@ export function renderPaymentSummary(){
   const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
   const taxCents = totalBeforeTaxCents * 0.1;
   const totalCents = totalBeforeTaxCents + taxCents;
-  
+
   const paymentSummaryHTML = `
       <div class="payment-summary-title">
         Order Summary
@@ -58,12 +59,37 @@ export function renderPaymentSummary(){
         </div>
       </div>
 
-      <button class="place-order-button button-primary">
+      <button class="place-order-button button-primary js-place-order">
         Place your order
       </button>
         
   `;
 
-  document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
-  
-};
+  document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHTML;
+
+  document
+    .querySelector(".js-place-order")
+    .addEventListener("click", async () => {
+      try {
+        const response = await fetch("https://supersimplebackend.dev/orders", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cart: cart,
+          }),
+        });
+
+        const order = await response.json();
+        addOrder(order);
+
+        //we can also use local storage to save the order instead of using a backend. We can save the order in local storage and then retrieve it later.
+        //localStorage.setItem("order", JSON.stringify(order));
+      } catch (error) {
+        console.log("Error placing order. Please try again later.");
+      }
+
+      window.location.href = "orders.html";
+    });
+}
