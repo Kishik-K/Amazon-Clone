511d99729d64203daa8ff0e0e7d4181c1ebb5fc7
Kishik <kishikkhajuria03@gmail.com>
2026-04-04 17:27:28 +0530

    convert loadproducts from XHR to FETCH

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -58,6 +58,30 @@ class Clothing extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch("https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productsData) => {
+      products = productsData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        }
+        return new Product(productDetails);
+      });
+
+      console.log("Load Products");
+    });
+  return promise;
+
+  /* //we can return a promise from this function and then use .then() to keep attaching more steps to the same promise.
+  loadProductsFetch().then(() => {
+    console.log("NEXT STEP");
+  });
+  */
+}
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
